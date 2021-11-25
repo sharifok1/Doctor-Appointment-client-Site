69c0a050d8511c69d074410a9727b6b1a0b6c4c2
sharifok1 <sharifok1@gmail.com>
2021-11-25 17:11:29 +0600

    add banner text

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -19,7 +19,7 @@ const Banner = () => {
           <Grid item xs={12} md={5}>
             <Typography style={{textAlign:'start'}}>
                 <h2>Start your smile</h2>
-                <p>Keep your smiling all day, We are the best pratner for your best services. Distinctio temporibus voluptas facilis nobis iure aliquid voluptatum aperiam vel repudiandae officia!</p>
+                <p>Keep your smiling all day, We are the best pratner for your best services. Just take an appointment and meet woth our doctor</p>
                 <Button variant="contained" style={{backgroundColor:'green'}}>take a service</Button>
             </Typography>
           </Grid>
